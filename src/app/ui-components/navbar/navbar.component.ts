69a5f31d02b12f7014927337c6a050686472f78a
jevonne876 <jevonne63@gmail.com>
2022-10-07 14:10:28 -0500

    hamburger menu added

--- src/app/ui-components/navbar/navbar.component.ts
+++ src/app/ui-components/navbar/navbar.component.ts
@@ -7,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  isLoggedIn:boolean = false;
+  isLoggedIn: boolean = false;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  navToggle() {
+    document.getElementById("menu-btn")?.classList.toggle("open");
+    document.getElementById("menu")?.classList.toggle("flex");
+    document.getElementById("menu")?.classList.toggle("hidden");
+
+  }
+
+
+
 
 
 }
