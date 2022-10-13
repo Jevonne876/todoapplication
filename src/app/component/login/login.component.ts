30747cd19be91f56c1b9a1a93df6ad3ef50f9c83
jevonne876 <jevonne63@gmail.com>
2022-10-13 11:54:56 -0500

    user login working

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user-model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  user: User = new User();
+
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+
+  userLogin() {
+    this.user.username = this.loginForm.value.username!;
+    this.user.password = this.loginForm.value.password!;
+
+    this.authenticationService.userLogin(this.user).subscribe((response: any) => {
+
+      console.log(response.body);
+      alert(`it worked`);
+
+    })
+
   }
 
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { observable, Observable } from 'rxjs';
 import { User } from '../models/user-model';
 import { environment } from 'src/environments/environment';
 
@@ -16,4 +16,10 @@ export class AuthenticationService {
   registerUser(user: User): Observable<User | HttpErrorResponse> {
     return this.http.post<User | HttpErrorResponse>(`${this.api}post/register-new-user`, user);
   }
+
+  userLogin(user: User): Observable<HttpResponse<User>> {
+    return this.http.post<User>(`${this.api}post/login`, user, { observe: 'response' })
+  }
 }
+
+{ observe: 'response' }
